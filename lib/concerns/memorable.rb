626ff82bab5ec7fc687e0349c6476ca6622d8c42
Chloe Foronda <caforonda103@yahoo.com>
2020-02-06 20:40:58 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,12 +1,17 @@
 module Memorable
-  
-  def reset_all
-    self.all.clear
-  end
+  module ClassMethods
+    def reset_all
+      self.all.clear
+    end
 
-  def count
-    self.all.count
+    def count
+      self.all.count
+    end
   end
+  
+  module InstanceMethods
+    def initialize
+    end
   
   
 end
